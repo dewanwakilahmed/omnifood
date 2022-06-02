c7cb3d80c819a74064dd2823331621e228aa3059
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-06-02 14:32:58 +0600

    Implementing a Sticky Navigation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 ///////////////////////////////////////
 // Make mobile navigation work
-
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-
 ///////////////////////////////////////
 
 ///////////////////////////////////////
 // Smooth scrolling animation
-
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
@@ -40,5 +37,29 @@ allLinks.forEach(function (link) {
     }
   });
 });
+///////////////////////////////////////
 
+///////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////
